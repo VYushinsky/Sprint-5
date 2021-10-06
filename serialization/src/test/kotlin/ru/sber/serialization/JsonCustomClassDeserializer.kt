d18e05dda3e818c6c22a406997916c9a08f3ec14
VYushinsky <78491821+VYushinsky@users.noreply.github.com>
2021-10-06 22:44:39 +0300

    Add files via upload

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,13 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -11,7 +18,12 @@ class JsonCustomClassDeserializer {
     fun `Нобходимо десериализовать данные в класс`() {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
+
+        val simpleModule = SimpleModule()
+            .addDeserializer(Client7::class.java, Client7Deserelizatoon())
+
         val objectMapper = ObjectMapper()
+            .registerModule(simpleModule)
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -20,5 +32,13 @@ class JsonCustomClassDeserializer {
         assertEquals("Иван", client.firstName)
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
+    }
+}
+
+class Client7Deserelizatoon: StdDeserializer<Client7>(Client7::class.java) {
+    override fun deserialize(p0: JsonParser?, p1: DeserializationContext?): Client7 {
+        val node: JsonNode? = p0?.getCodec()?.readTree(p0)
+        val itemName = node?.get("client").toString().trim('"').split(" ")
+        return Client7(itemName[1], itemName[0],itemName[2])
     }
 }
--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomNamingPropertyStrategyTest.kt
@@ -1,6 +1,10 @@
 package ru.sber.serialization
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -14,7 +18,7 @@ class JsonCustomNamingPropertyStrategyTest {
         val data =
             """{"FIRSTNAME": "Иван", "LASTNAME": "Иванов", "MIDDLENAME": "Иванович", "PASSPORTNUMBER": "123456", "PASSPORTSERIAL": "1234", "BIRTHDATE": "1990-01-01"}"""
         val objectMapper = ObjectMapper()
-
+            .registerModules(KotlinModule(), JavaTimeModule()).setPropertyNamingStrategy(NamingStrategy())
         // when
         val client = objectMapper.readValue<Client1>(data)
 
@@ -26,4 +30,13 @@ class JsonCustomNamingPropertyStrategyTest {
         assertEquals("1234", client.passportSerial)
         assertEquals(LocalDate.of(1990, 1, 1), client.birthDate)
     }
+
+    class NamingStrategy: PropertyNamingStrategies.NamingBase(){
+        override fun translate(p0: String?): String {
+            return p0?.uppercase()!!
+        }
+
+    }
 }
+
+
